4e5c09f3b4b8170c1ae69cf3712b5fea8fe45187
Philip Yu <jialeyu@gmail.com>
2020-04-01 15:13:29 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,17 +2,21 @@ console.log("Up and running!");
 
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
-let cardOne = cards[0];
-let cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
+function checkForMath(){
+	if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1])
 		alert("You found a match!");
 	else 
 		alert("Sorry, try again");
 }
+}
+
+function flipCard (cardID){
+	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+	checkForMath();
+}
+
+flipCard(0);
+flipCard(1);
